4ca6cf1441f076621463f657d289a9dd1e34415c
bstets1 <52285717+bstets1@users.noreply.github.com>
2022-09-27 15:15:55 -0400

    Update create-bacpacs.sh

--- container/Docker/create-bacpacs.sh
+++ container/Docker/create-bacpacs.sh
@@ -2,7 +2,7 @@
 
 for i in {1..60};
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD  -Q "SELECT Name FROM SYS.DATABASES"
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD  -Q "SELECT Name FROM SYS.DATABASES"
     if [ $? -eq 0 ]
     then
         echo "sql server ready"
@@ -13,8 +13,8 @@ do
     fi
 done
 
-/opt/mssql-tools/bin/sqlcmd -l 300 -S localhost -U sa -P $SA_PASSWORD -d master -i "/create_procedure_restoreheaderonly.sql"
-/opt/mssql-tools/bin/sqlcmd -l 300 -S localhost -U sa -P $SA_PASSWORD -d master -i "/create_procedure_restoredatabase.sql"
+/opt/mssql-tools/bin/sqlcmd -l 300 -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i "/create_procedure_restoreheaderonly.sql"
+/opt/mssql-tools/bin/sqlcmd -l 300 -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i "/create_procedure_restoredatabase.sql"
 #run the setup script to create the DB and the schema in the DB
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
 for f in /mnt/external/*.bak;
@@ -23,7 +23,7 @@ do
     name="${s%.*}"
     extension="${s#*.}"
     echo "Restoring $f..."
-    /opt/mssql-tools/bin/sqlcmd -l 300 -S localhost -U sa -P $SA_PASSWORD -d master -q "EXEC dbo.restoredatabase '/mnt/external/$name.$extension', '$name'"
+    /opt/mssql-tools/bin/sqlcmd -l 300 -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -q "EXEC dbo.restoredatabase '/mnt/external/$name.$extension', '$name'"
     echo "Creating bacpac..."
-    /sqlpackage/sqlpackage -a:"Export" -ssn:"localhost" -su:"sa" -sp:"$SA_PASSWORD" -sdn:"$name" -tf:"/mnt/external/$name.bacpac"
+    /sqlpackage/sqlpackage -a:"Export" -ssn:"localhost" -su:"sa" -sp:"$MSSQL_SA_PASSWORD" -sdn:"$name" -tf:"/mnt/external/$name.bacpac"
 done
